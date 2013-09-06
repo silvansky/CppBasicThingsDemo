de014c8299e4e21309402f87ac8045464a2dbd27
silvansky <v.silvansky@gmail.com>
2013-09-06 11:33:34 +0400

    Initial Commit

---
+++ iotests/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  iotests
+//
+//  Created by Valentine on 06.09.13.
+//  Copyright (c) 2013 silvansky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
